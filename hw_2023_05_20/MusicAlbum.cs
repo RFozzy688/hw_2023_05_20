5a41c1832a337c3240cbb1e7e28c33472b2e5ead
RFozzy688 <pokerfozzy@gmail.com>
2023-05-23 08:14:34 +0300

    start project

---
+++ hw_2023_05_20/MusicAlbum.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw_2023_05_20
+{
+    internal class MusicAlbum
+    {
+    }
+}
